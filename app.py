ad121a6d455a7c24201c119c1b4766d0f7eab246
Cristian Anjari <cristian.anjari@usach.cl>
2022-06-24 17:11:51 -0400

    Update app.py
    register_blueprints() changed to the correct use of register_blueprint()

--- app.py
+++ app.py
@@ -7,21 +7,22 @@ def create_app():
     app.config.from_object(config['development'])
 
     register_extensions(app)
-    register_blueprints(app)
+    register_blueprint(app)
     return app
 
 def register_extensions(app):
     db.init_app(app)
 
-def register_blueprints(app):
+def register_blueprint(app):
     #Importa las blueprints
     from src.blueprints.auth import auth
     from src.blueprints.usuario import usuario
 
     # Registra las rutas en la app
-    app.register_blueprints(auth)
-    app.register_blueprints(usuario)
+    app.register_blueprint(auth)
+    app.register_blueprint(usuario)
 
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
+    
